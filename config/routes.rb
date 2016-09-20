c4ced13b7445b2ce83afb9c70e4fb3ea9028ddd9
reggiebarnett <reggiedbarnett@gmail.com>
2016-09-20 15:25:12 -0400

    added nav bar and some testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root 'static#home'
-
   get '/projects',	to: 'static#projects'
   get '/feed',		to: 'static#feed'	
   get '/comments',	to: 'static#comments'
-
 end
